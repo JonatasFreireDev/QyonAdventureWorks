fee88960eb4f1a56db72384bb5fde1d02f6acbd2
Jonatas <jonatasf1997@hotmail.com>
2022-01-03 00:46:02 -0300

    list

--- frontend/src/pages/Competitors/index.tsx
+++ frontend/src/pages/Competitors/index.tsx
@@ -1,16 +1,61 @@
-import Button from "@mui/material/Button";
+import { useCallback, useEffect, useState } from "react";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+
 import Header from "../../components/Header";
+import { ICompetitors } from "../../interfaces/ICompetitors";
 
 import * as S from "./styles";
+import api from "../../services/api";
+import { columnsCompetitor } from "../../utils/tables/columnsCompetitors";
 
 const Competitors: React.FC = () => {
+  const [competitors, setCompetitors] = useState<ICompetitors[]>([]);
+
+  useEffect(() => {
+    getCompetitors();
+  }, []);
+
+  const getCompetitors = useCallback(async () => {
+    const { data } = await api.get<ICompetitors[]>("/competitors");
+
+    if (data!) {
+      setCompetitors([...data]);
+    }
+  }, []);
+
+  const putCompetitors = useCallback(async (params) => {
+    const competitor = params.row;
+
+    const { data } = await api.put(`/competitors/${params.id}`, competitor);
+
+    await getCompetitors();
+    console.log(data);
+  }, []);
+
   return (
     <>
       <Header title="Competitors" />
       <S.Container>
-        <div>
-          <h1>aew</h1>
-          <Button variant="contained"> Olá Mundo</Button>
+        <div style={{ height: 600, width: "auto", padding: 50 }}>
+          <DataGrid
+            rows={competitors}
+            columns={columnsCompetitor}
+            pageSize={5}
+            rowsPerPageOptions={[5]}
+            localeText={{
+              toolbarDensity: "Size",
+              toolbarDensityLabel: "Size",
+              toolbarDensityCompact: "Small",
+              toolbarDensityStandard: "Medium",
+              toolbarDensityComfortable: "Large",
+            }}
+            components={{
+              Toolbar: GridToolbar,
+            }}
+            onCellEditCommit={(params) => {
+              putCompetitors(params);
+            }}
+          />
         </div>
       </S.Container>
     </>
--- frontend/src/pages/Competitors/styles.ts
+++ frontend/src/pages/Competitors/styles.ts
@@ -2,13 +2,10 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   margin-left: 80px;
-
-  p {
-    text-align: center;
-  }
+  display: flex;
+  flex-direction: column;
   
-  span {
-    margin-top: 10px;
-    font-size: 16px;
+  div {
+    /* background: grey; */
   }
 `;
